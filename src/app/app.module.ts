2162f9f61d98e39390a85be41e8d9961ffb620e7
Sameer <sameerajmani@outlook.com>
2019-08-19 11:35:54 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { LoginComponent } from './component/login/login.component';
+import { OrdersComponent } from './component/orders/orders.component';
+import { CustomersComponent } from './component/customers/customers.component';
+import { EditComponent } from './component/edit/edit.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ListViewComponent } from './component/list-view/list-view.component';
+ import { ToastrModule } from 'ngx-toastr';
+import { RegisterComponent } from './component/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    OrdersComponent,
+    CustomersComponent,
+    EditComponent,
+    ListViewComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+     ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
